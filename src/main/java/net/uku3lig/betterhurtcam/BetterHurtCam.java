389aaed4e6233ced8faf6284a3a9bdd667a8398f
uku <hi@uku.moe>
2025-09-30 18:43:37 +0200

    feat: 1.21.9

--- src/main/java/net/uku3lig/betterhurtcam/BetterHurtCam.java
+++ src/main/java/net/uku3lig/betterhurtcam/BetterHurtCam.java
@@ -6,17 +6,18 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 import net.uku3lig.betterhurtcam.config.BHCConfig;
 import net.uku3lig.ukulib.config.ConfigManager;
 import net.uku3lig.ukulib.utils.Ukutils;
 import org.lwjgl.glfw.GLFW;
 
 public class BetterHurtCam implements ModInitializer {
-    public static final String MOD = "BetterHurtCam";
+    private static final KeyBinding.Category CATEGORY = KeyBinding.Category.create(Identifier.of("betterhurtcam", "key"));
 
-    private static final KeyBinding toggle = new KeyBinding("key.betterhurtcam.toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F8, MOD);
-    private static final KeyBinding plus = new KeyBinding("key.betterhurtcam.plus", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F7, MOD);
-    private static final KeyBinding minus = new KeyBinding("key.betterhurtcam.minus", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F6, MOD);
+    private static final KeyBinding toggle = new KeyBinding("key.betterhurtcam.toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F8, CATEGORY);
+    private static final KeyBinding plus = new KeyBinding("key.betterhurtcam.plus", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F7, CATEGORY);
+    private static final KeyBinding minus = new KeyBinding("key.betterhurtcam.minus", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F6, CATEGORY);
 
     @Getter
     private static final ConfigManager<BHCConfig> manager = ConfigManager.createDefault(BHCConfig.class, "betterhurtcam");
